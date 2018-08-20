6c74f8ea761b7137cd5de3cf551aa79fe25a6766
sk <skp.guptha@gmail.com>
2018-08-20 09:25:10 +0530

    first commit

---
+++ Webservces/api/views.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from api.serializers import *
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import *
+from rest_framework import status
+
+@api_view(['POST','GET'])
+def add_county(request):
+    country_serializer = CountrySerializer(data=request.data)
+    if request.method == 'POST':
+        if country_serializer.is_valid():
+            country_serializer.save()
+            return Response({"data": "Country added successfully"}, status=status.HTTP_201_CREATED)
+        else:
+            error_details = []
+            for key in country_serializer.errors.keys():
+                error_details.append({"field": key, "message": country_serializer.errors[key][0]})
+
+            data = {
+                    "Error": {
+                        "status": 400,
+                        "message": "Your submitted data was not valid - please correct the below errors",
+                        "error_details": error_details
+                        }
+                    }
+
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        snippets = Country.objects.all()
+        country_serializer = CountrySerializer(snippets, many=True).data
+        return Response({"data": country_serializer}, status=status.HTTP_200_OK)
+
+
+
+
+@api_view(['GET', 'POST','DELETE'])
+def update_county(request, id):
+    country_obj = Country.objects.get(id=id)
+
+    if request.method == "GET":
+        country_data = CountrySerializer(country_obj).data
+        return Response({"data": country_data}, status=status.HTTP_200_OK)
+    elif request.method == "DELETE":
+        country_obj.delete()
+        return Response({"data": "Country Deleted Successfully."}, status=status.HTTP_200_OK)
+    else:
+        country_serializer = CountrySerializer(country_obj, data=request.data)
+        if country_serializer.is_valid():
+            country_serializer.save()
+            return Response({"data": "Country Updated successfully"}, status=status.HTTP_200_OK)
+        else:
+            error_details = []
+            for key in country_serializer.errors.keys():
+                error_details.append({"field": key, "message": country_serializer.errors[key][0]})
+            data = {
+                    "Error": {
+                        "status": 400,
+                        "message": "Your submitted data was not valid - please correct the below errors",
+                        "error_details": error_details
+                        }
+                    }
+
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
